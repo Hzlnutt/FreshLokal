ba5a122855ab41a98d81c1ab96f2fb47f548422c
Misba <devamisbakhulk07@gmail.com>
2025-04-17 10:09:06 +0700

    coba up

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,7 +23,7 @@
             <div class="d-flex justify-content-between align-items-center mb-4">
                 <div>
                     <h4 class="mb-1">
-                        <i class="fas fa-store me-2"></i> FreshLokal Marketplace
+                        <i class="fas fa-store me-2"></i> FreshLokal Marketplaces
                     </h4>
                     <p class="text-muted mb-0">Temukan produk segar dan berkualitas</p>
                 </div>
